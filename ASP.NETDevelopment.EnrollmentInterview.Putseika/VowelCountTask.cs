fe5ee432dbf2c56e0b1bb8a985a6de2ceb35584b
oldpande <49763380+oldpande@users.noreply.github.com>
2019-07-19 16:03:08 +0300

    second solution

---
+++ ASP.NETDevelopment.EnrollmentInterview.Putseika/VowelCountTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ASP.NETDevelopment.EnrollmentInterview.Putseika
+{
+    class VowelCountTask
+    {
+        private static char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+
+        public static int CountOfVowels(string inputString)
+        {
+            int countOfVowels = 0;
+            foreach (char symbol in inputString)
+            {
+                for (int i = 0; i < vowels.Length; i++)
+                {
+                    if (symbol == vowels[i])
+                        countOfVowels++;
+                }
+            }
+            return countOfVowels;
+        }
+    }
+}
