8965da984dfcaf2ab71c6b9b314b301413199b2a
oldpande <49763380+oldpande@users.noreply.github.com>
2019-07-19 16:27:33 +0300

    Task number 3 completed

---
+++ ASP.NETDevelopment.EnrollmentInterview.Putseika/Task_3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ASP.NETDevelopment.EnrollmentInterview.Putseika
+{
+    class Task_3
+    {
+        public static void OrderStringsByLength(string[] arrOfString)
+        {
+            string swap = "";
+            for (int i = 0; i < arrOfString.Length; i++)
+            {
+                for (int j = arrOfString.Length - 1; j > i; j--)
+                    if (arrOfString[j - 1].Length > arrOfString[j].Length)
+                    {
+                        swap = arrOfString[j - 1];
+                        arrOfString[j - 1] = arrOfString[j];
+                        arrOfString[j] = swap;
+                    }
+            }
+        }
+
+    }
+}
